57cbef10b50d021f05f3d7cb2b51b6f14477d05c
Crobe182 <rob23052@byui.edu>
2024-04-27 21:59:33 -0600

    finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercent = Console.ReadLine();
+        int gradeNumber = int.Parse(gradePercent);
+
+        string letterGrade = ("");
+
+        if (gradeNumber >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, better luck next time.");
+        }
     }
 }
