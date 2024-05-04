4eea2bc80e48592f811e3a868a30e2a07482f27a
Crobe182 <rob23052@byui.edu>
2024-05-03 23:33:21 -0600

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("A random number has been chosen! \n");
+
+        Random randomGenerator = new Random();
+
+        int randomNumber = randomGenerator.Next(1, 101);
+
+        int guessedNumber = (0);
+
+        while (guessedNumber != randomNumber)
+        {
+            Console.Write("What is your guess? ");
+
+            string guess = Console.ReadLine();
+
+            guessedNumber = int.Parse(guess);
+
+            if (guessedNumber > randomNumber)
+            {
+            Console.Write("lower \n");
+            }
+            if (guessedNumber < randomNumber) 
+            {
+            Console.Write("higher \n");
+            }
+            if (guessedNumber == randomNumber)
+            {
+            Console.Write("Congrats! you guessed the magic number! \n");
+            }
+        }
     }
 }
